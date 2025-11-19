77478207c5666a455a48f51d71d8df34d226f17d
dastan <chynybekuuludastan@gmail.com>
2025-11-19 10:20:18 +0600

    refactor: components in task_1 to use React.memo and PureComponent for performance optimization

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,47 +1,59 @@
-import { Component } from 'react';
+import React, { Component, PureComponent } from 'react'
 
 type IUser = {
-    name: string
-    age: number
+	name: string
+	age: number
 }
 
 type IProps = {
-    user: IUser
+	user: IUser
 }
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+const FirstComponent = React.memo(({ name, age }: IUser) => (
+	<div>
+		my name is {name}, my age is {age}
+	</div>
+))
 
 // functional component
 // Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+const SecondComponent = React.memo(
+	({ user: { name, age } }: IProps) => (
+		<div>
+			my name is {name}, my age is {age}
+		</div>
+	),
+	(prevProps, nextProps) =>
+		prevProps.user.name === nextProps.user.name &&
+		prevProps.user.age === nextProps.user.age
+)
 
 // class component
-class ThirdComponent extends Component<IUser> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.name}, my age is {this.props.age}
-            </div>
-        )
-    }
+class ThirdComponent extends PureComponent<IUser> {
+	render() {
+		return (
+			<div>
+				my name is {this.props.name}, my age is {this.props.age}
+			</div>
+		)
+	}
 }
 
 // class component
 class FourthComponent extends Component<IProps> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
-            </div>
-        )
-    }
+	shouldComponentUpdate(nextProps: IProps): boolean {
+		return (
+			this.props.user.name !== nextProps.user.name ||
+			this.props.user.age !== nextProps.user.age
+		)
+	}
+
+	render() {
+		return (
+			<div>
+				my name is {this.props.user.name}, my age is {this.props.user.age}
+			</div>
+		)
+	}
 }
